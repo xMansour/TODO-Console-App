68107579aae2c16d7f69f4030c406c063a7218f6
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-20 15:35:45 +0200

    Create clearScsreen

--- src/com/swe/todoconsoleapp/utils/MenuPrinter.java
+++ src/com/swe/todoconsoleapp/utils/MenuPrinter.java
@@ -38,4 +38,9 @@ public class MenuPrinter {
                 printToDo(toDo);
             }
     }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
