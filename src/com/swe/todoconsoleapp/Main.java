1f50ec22a3d60f186b612e0818f7c181b79eb8f0
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-20 15:24:10 +0200

    Finish filter by start and date

--- src/com/swe/todoconsoleapp/Main.java
+++ src/com/swe/todoconsoleapp/Main.java
@@ -1,28 +1,20 @@
 package com.swe.todoconsoleapp;
 
-import com.swe.todoconsoleapp.entity.ToDo;
-import com.swe.todoconsoleapp.service.ToDoService;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import com.swe.todoconsoleapp.entity.Criteria;
+import com.swe.todoconsoleapp.entity.ToDo;
+import com.swe.todoconsoleapp.exception.InvalidDateFormatException;
 import com.swe.todoconsoleapp.exception.PriorityNotFoundException;
 import com.swe.todoconsoleapp.exception.ToDoNotFoundException;
 import com.swe.todoconsoleapp.service.ToDoService;
 import com.swe.todoconsoleapp.utils.MenuPrinter;
+
+import java.util.List;
 
 public class Main {
 
     private static final ToDoService toDoService = new ToDoService();
 
     public static void main(String[] args) throws Exception {
-
-
         try {
             var selectedTitle = MenuPrinter.printFindByMenu(Criteria.TITLE.name());
             var selectedTodo = toDoService.findByTitle(selectedTitle);
@@ -30,6 +22,30 @@ public class Main {
         } catch (ToDoNotFoundException e) {
             System.out.println(e.getMessage());
         }
+
+        try {
+            var selectedPriority = MenuPrinter.printFindByMenu(Criteria.PRIORITY.name());
+            var selectedToDos = toDoService.findByPriority(selectedPriority.toUpperCase());
+            MenuPrinter.PrintToDos(selectedToDos);
+        } catch (PriorityNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            var selectedStartDate = MenuPrinter.printFindByMenu(Criteria.STARTDATE.name());
+            var selectedToDos = toDoService.findByDate(0, selectedStartDate);
+            MenuPrinter.PrintToDos(selectedToDos);
+        } catch (InvalidDateFormatException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            var selectedEndDate = MenuPrinter.printFindByMenu(Criteria.ENDDATE.name());
+            var selectedToDos = toDoService.findByDate(1, selectedEndDate);
+            MenuPrinter.PrintToDos(selectedToDos);
+        } catch (InvalidDateFormatException e) {
+            System.out.println(e.getMessage());
+        }
+
         var todo2 = new ToDo();
         todo2.setTitle("todo2");
         toDoService.writeToDo(todo2);
@@ -38,14 +54,7 @@ public class Main {
         todo3.setTitle("todo3");
         toDoService.writeToDo(todo3);
 
-        var selectedPriority = MenuPrinter.printFindByMenu(Criteria.PRIORITY.name());
 
-        try {
-            var selectedToDos = toDoService.findByPriority(selectedPriority.toUpperCase());
-            MenuPrinter.PrintToDos(selectedToDos);
-        } catch (PriorityNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
         List<ToDo> todos = toDoService.selectAllToDos();
         todos.forEach(todo -> {
             System.out.println(todo.getTitle());
