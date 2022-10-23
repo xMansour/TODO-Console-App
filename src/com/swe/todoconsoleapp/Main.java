85bc5854fa089bafbaa152343116b0adee214a03
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-23 11:46:00 +0200

    Create main menu

--- src/com/swe/todoconsoleapp/Main.java
+++ src/com/swe/todoconsoleapp/Main.java
@@ -11,38 +11,77 @@ public class Main {
 
     private static final ToDoService toDoService = new ToDoService();
 
-    public static void main(String[] args) throws Exception {
-        try {
-            var selectedTitle = MenuPrinter.printFindByMenu(Criteria.TITLE.name());
-            var selectedTodo = toDoService.findByTitle(selectedTitle);
-            MenuPrinter.printResults(selectedTodo);
-        } catch (ToDoNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] args) {
 
-        try {
-            var selectedPriority = MenuPrinter.printFindByMenu(Criteria.PRIORITY.name());
-            var selectedToDos = toDoService.findByPriority(selectedPriority.toUpperCase());
-            MenuPrinter.printResults(selectedToDos);
-        } catch (PriorityNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+        var result = MenuPrinter.printMainMenu();
+        switch (result) {
+            case 1: // Create
+                break;
+            case 2: // Update
+                break;
+            case 3: // Delete
+                break;
+            case 4: // Show All
+                break;
+            case 5: // Show Top 5
+                break;
+            case 6: // Filter by title
+            {
+                try {
+                    var selectedTitle = MenuPrinter.printFindByMenu(Criteria.TITLE.name());
+                    var selectedTodo = toDoService.findByTitle(selectedTitle);
+                    MenuPrinter.printResults(selectedTodo);
+                    MenuPrinter.resetMenu();
+                } catch (ToDoNotFoundException e) {
+                    System.out.println(e.getMessage());
+                    MenuPrinter.resetMenu();
+                }
+            }
+            break;
+            case 7: // Filter by start date
+            {
+                try {
+                    var selectedStartDate = MenuPrinter.printFindByMenu(Criteria.START_DATE.name());
+                    var selectedToDos = toDoService.findByDate(0, selectedStartDate);
+                    MenuPrinter.printResults(selectedToDos);
+                    MenuPrinter.resetMenu();
+                } catch (InvalidDateFormatException e) {
+                    System.out.println(e.getMessage());
+                    MenuPrinter.resetMenu();
+                }
+            }
+            break;
+            case 8: // Filter by end date
+            {
+                try {
+                    var selectedStartDate = MenuPrinter.printFindByMenu(Criteria.START_DATE.name());
+                    var selectedToDos = toDoService.findByDate(1, selectedStartDate);
+                    MenuPrinter.printResults(selectedToDos);
+                    MenuPrinter.resetMenu();
+                } catch (InvalidDateFormatException e) {
+                    System.out.println(e.getMessage());
+                    MenuPrinter.resetMenu();
+                }
+            }
+            break;
+            case 9: // Filter by priority
+            {
+                try {
+                    var selectedPriority = MenuPrinter.printFindByMenu(Criteria.PRIORITY.name());
+                    var selectedToDos = toDoService.findByPriority(selectedPriority.toUpperCase());
+                    MenuPrinter.printResults(selectedToDos);
+                    MenuPrinter.resetMenu();
 
-        try {
-            var selectedStartDate = MenuPrinter.printFindByMenu(Criteria.START_DATE.name());
-            var selectedToDos = toDoService.findByDate(0, selectedStartDate);
-            MenuPrinter.printResults(selectedToDos);
-        } catch (InvalidDateFormatException e) {
-            System.out.println(e.getMessage());
+                } catch (PriorityNotFoundException e) {
+                    System.out.println(e.getMessage());
+                    MenuPrinter.resetMenu();
+                }
+            }
+            break;
+            case 10: // Exit
+                System.exit(0);
+                break;
         }
-        try {
-            var selectedEndDate = MenuPrinter.printFindByMenu(Criteria.END_DATE.name());
-            var selectedToDos = toDoService.findByDate(1, selectedEndDate);
-            MenuPrinter.printResults(selectedToDos);
-        } catch (InvalidDateFormatException e) {
-            System.out.println(e.getMessage());
-        }
-
     }
 
 }
--- src/com/swe/todoconsoleapp/utils/MenuPrinter.java
+++ src/com/swe/todoconsoleapp/utils/MenuPrinter.java
@@ -6,8 +6,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MenuPrinter {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static Integer printMainMenu() {
+        String input = "";
+        do {
+            System.out.println("Please select from menu:");
+            System.out.println("1- Create todo.");
+            System.out.println("2- Edit todo.");
+            System.out.println("3- Delete todo.");
+            System.out.println("4- Show all todos.");
+            System.out.println("5- Show top nearest 5 todos.");
+            System.out.println("6- Filter by title.");
+            System.out.println("7- Filter by start date.");
+            System.out.println("8- Filter by end date.");
+            System.out.println("9- Filter by priority.");
+            System.out.println("10- Exit");
+            input = scanner.nextLine();
+        } while (!InputValidator.isValidNumber(input, 1, 10));
+        return Integer.parseInt(input);
+    }
+
     public static String printFindByMenu(String criteria) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter todo " + criteria + ": ");
         return scanner.nextLine();
     }
@@ -25,22 +46,20 @@ public class MenuPrinter {
             System.out.println("priority: Not assigned yet");
             System.out.println("category: Not assigned yet");
         }
-
         System.out.println("--------");
     }
 
     public static void printResults(List<ToDo> toDos) {
-        if (toDos.size() == 0)
-            System.out.println("No toDos found");
-        else
-            for (var toDo : toDos
-            ) {
-                printResults(toDo);
-            }
+        if (toDos.size() == 0) System.out.println("No toDos found");
+        else for (var toDo : toDos) {
+            printResults(toDo);
+        }
     }
 
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    public static void resetMenu() {
+        System.out.println("==========");
+        System.out.println("press ENTER to return to main menu");
+        scanner.nextLine();
+        printMainMenu();
     }
 }
