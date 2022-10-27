fff2b3eaddef709f505522494ffb5709b62e702d
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-27 17:28:52 +0200

    Remove unused classes

--- src/com/swe/todoconsoleapp/exception/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.swe.todoconsoleapp.exception;
-
-public class CategoryNotFoundException extends Exception {
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/com/swe/todoconsoleapp/exception/PriorityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.swe.todoconsoleapp.exception;
-
-public class PriorityNotFoundException extends Exception {
-    public PriorityNotFoundException(String message) {
-        super(message);
-    }
-}
